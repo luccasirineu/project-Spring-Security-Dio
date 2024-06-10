248f0b753060faf30c8c59895513f5af6bb0b1ed
luccasirineu <luccas19100@outlook.com>
2024-06-10 14:36:51 -0300

    criação do projeto e configuração banco de dados

---
+++ src/main/java/com/br/spring_security_dio/SpringSecurityDioApplication.java
@@ -0,0 +1,13 @@
+package com.br.spring_security_dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/spring_security_dio/SpringSecurityDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.spring_security_dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
